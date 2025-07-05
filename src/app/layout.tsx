4c6fe965d250b42d7fb1ed92b067d9e16eeadac0
Julia Tito <94010304+TryMySeMfQ@users.noreply.github.com>
2025-07-05 15:13:48 -0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,8 @@
 // src/app/layout.tsx
 import './styles/globals.css';
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
 import { Navbar } from './components/ui/Navbar/Navbar';
 
-const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Segurança Feminina',
