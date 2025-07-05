c0d983c645b8e26b3089dce5eb4c05837829fa16
Julia Tito <94010304+TryMySeMfQ@users.noreply.github.com>
2025-07-05 15:28:55 -0300

    Update postcss.config.mjs

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,12 +1,6 @@
 const config = {
   plugins: ["@tailwindcss/postcss"],
+  autoprefixer: {},
 };
 
 export default config;
-
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
